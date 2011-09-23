f19888c6d8cba03a0d2a1aed56e90e724209e5e8
Owain <owain.perry@thetrainline.com>
2011-09-23 22:18:18 +0100

    first commit

---
+++ build.bat
@@ -0,0 +1,2 @@
+regsvr32 /s tools\Skype4COM\Skype4COM.dll
+tools\nant\bin\nant -f:component.build -l:BuildOutput.txt %1
---
+++ test/CCSkype.UnitTests/Class1.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace CCSkype.UnitTests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Should_pass()
+        {
+            Assert.That(true);
+        }
+    }
+}
