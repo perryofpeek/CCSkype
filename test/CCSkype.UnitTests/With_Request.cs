890428b6d9cfa7252107a14de92595f5be4acd53
Owain <owain.perry@thetrainline.com>
2011-09-29 14:14:10 +0100

    tidy up and build script working on .net 4 vs 2010

--- test/CCSkype.UnitTests/With_Request.cs
+++ test/CCSkype.UnitTests/With_Request.cs
@@ -2,16 +2,17 @@
 
 namespace CCSkype.UnitTests
 {
+    // ReSharper disable InconsistentNaming
     [TestFixture]
-    public class HTTPGetTests
+    public class With_Request
     {
-        [Test]
-        public void ShouldGetWebDocument()
+        [Test, Ignore("Write this properly or use a better http library")]
+        public void Should_Get_Web_Document()
         {
-            var httpGet = new HttpGet(20,"","");
+            var httpGet = new HttpGet(20, "", "");
             httpGet.Request("http://www.google.co.uk");
             var document = httpGet.ResponseBody;
             Assert.IsTrue(document.Contains("google"));
-        }                  
+        }
     }
 }
--- test/CCSkype.UnitTests/controller/With_Start.cs
+++ test/CCSkype.UnitTests/controller/With_Start.cs
@@ -1,10 +1,4 @@
-﻿using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using Castle.MicroKernel.Registration;
-using Castle.Windsor;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
 namespace CCSkype.UnitTests.controller
 {
@@ -12,16 +6,6 @@ namespace CCSkype.UnitTests.controller
     [TestFixture]
     public class With_Start
     {
-        [SetUp]
-        public void SetUp()
-        {
-        }
-
-        [TearDown]
-        public void TearDown()
-        {
-        }
-
         [Test, Ignore("Debug only")]
         public void test()
         {
@@ -32,16 +16,6 @@ namespace CCSkype.UnitTests.controller
             controller.HttpTimeout = 30;
             controller.Configuration = @"TestData/Config/RealUsers.xml";
             controller.Start();
-        }
-
-
-        [Test]
-        public void Should_Start_running_the_application()
-        {
-            var container = new WindsorContainer();
-            container.Register(AllTypes.FromAssembly(Assembly.GetAssembly(typeof(Timer))).Where(t => t.GetInterfaces().Any()).Configure(component => component.LifeStyle.Transient).WithService.FirstInterface());
-            var timer = container.Resolve<ITimer>();
-            timer.Start();
-        }
+        }     
     }
 }
